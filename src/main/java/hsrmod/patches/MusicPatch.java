434d5daf919e9919f7ec147e77876c368668872f
BowenJXD <bjia0018@student.monash.edu>
2024-11-28 16:51:25 +1100

    解决了MusicPatch和其他mod crash的问题。

--- src/main/java/hsrmod/patches/MusicPatch.java
+++ src/main/java/hsrmod/patches/MusicPatch.java
@@ -2,6 +2,8 @@ package hsrmod.patches;
 
 import com.badlogic.gdx.audio.Music;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpireReturn;
 import com.megacrit.cardcrawl.audio.MainMusic;
 import com.megacrit.cardcrawl.audio.TempMusic;
 
@@ -13,40 +15,32 @@ public class MusicPatch {
     public MusicPatch() {
     }
 
-    public static Music Replace(TempMusic t, String key) {
+    @SpirePrefixPatch
+    public static SpireReturn<Music> Prefix(TempMusic t, String key) {
+        Music music = null;
         switch (key) {
-            case "SHOP":
-                return MainMusic.newMusic("audio/music/STS_Merchant_NewMix_v1.ogg");
-            case "SHRINE":
-                return MainMusic.newMusic("audio/music/STS_Shrine_NewMix_v1.ogg");
-            case "MINDBLOOM":
-                return MainMusic.newMusic("audio/music/STS_Boss1MindBloom_v1.ogg");
-            case "BOSS_BOTTOM":
-                return MainMusic.newMusic("audio/music/STS_Boss1_NewMix_v1.ogg");
-            case "BOSS_CITY":
-                return MainMusic.newMusic("audio/music/STS_Boss2_NewMix_v1.ogg");
-            case "BOSS_BEYOND":
-                return MainMusic.newMusic("audio/music/STS_Boss3_NewMix_v1.ogg");
-            case "BOSS_ENDING":
-                return MainMusic.newMusic("audio/music/STS_Boss4_v6.ogg");
-            case "ELITE":
-                return MainMusic.newMusic("audio/music/STS_EliteBoss_NewMix_v1.ogg");
-            case "CREDITS":
-                return MainMusic.newMusic("audio/music/STS_Credits_v5.ogg");
             case "RobinBGM":
-                return MainMusic.newMusic("HSRModResources/audio/music/RobinBGM.mp3");
+                music = MainMusic.newMusic("HSRModResources/audio/music/RobinBGM.mp3");
+                break;
             case "End of the Eternal Freeze_1":
-                return MainMusic.newMusic("HSRModResources/audio/music/Wildfire_1.mp3");
+                music = MainMusic.newMusic("HSRModResources/audio/music/Wildfire_1.mp3");
+                break;
             case "End of the Eternal Freeze_2":
-                return MainMusic.newMusic("HSRModResources/audio/music/Wildfire_2.mp3");
+                music = MainMusic.newMusic("HSRModResources/audio/music/Wildfire_2.mp3");
+                break;
             case "Divine Seed_1":
-                return MainMusic.newMusic("HSRModResources/audio/music/PedujaraEvenImmortalityEnds.mp3");
+                music = MainMusic.newMusic("HSRModResources/audio/music/PedujaraEvenImmortalityEnds.mp3");
+                break;
             case "Divine Seed_2":
-                return MainMusic.newMusic("HSRModResources/audio/music/PedujaraDemiselessExistence.mp3");
+                music = MainMusic.newMusic("HSRModResources/audio/music/PedujaraDemiselessExistence.mp3");
+                break;
             case "Salutations of Ashen Dreams":
-                return MainMusic.newMusic("HSRModResources/audio/music/HopeIsTheThingWithFeathers.mp3");
-            default:
-                return MainMusic.newMusic("audio/music/" + key);
+                music = MainMusic.newMusic("HSRModResources/audio/music/HopeIsTheThingWithFeathers.mp3");
+                break;
         }
+        if (music == null)
+            return SpireReturn.Continue();
+        else
+            return SpireReturn.Return(music);
     }
 }

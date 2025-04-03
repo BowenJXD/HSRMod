3ad077b4e468cc269d087b7863bf27c7b2ccea7a
BowenJXD <aaronjiang12138@gmail.com>
2025-04-03 18:44:38 +0800

    增加了一些测试玩家画风。 增加了银河铁道之夜事件。 修改了【因为我们是家人】的效果。 增加了丰饶的部分卡图。 增加了英语的本地化。

---
+++ src/main/java/hsrmod/cardsV2/NightOnTheMilkyWay.java
@@ -0,0 +1,56 @@
+package hsrmod.cardsV2;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardAtBottomOfDeckAction;
+import com.megacrit.cardcrawl.actions.common.MakeTempCardInDrawPileAction;
+import com.megacrit.cardcrawl.cards.curses.Normality;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.map.MapEdge;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.rooms.*;
+import hsrmod.cards.BaseCard;
+import hsrmod.modcore.HSRMod;
+import hsrmod.signature.utils.SignatureHelper;
+import hsrmod.utils.RelicEventHelper;
+
+public class NightOnTheMilkyWay extends BaseCard {
+    public static final String ID = NightOnTheMilkyWay.class.getSimpleName();
+    
+    public NightOnTheMilkyWay() {
+        super(ID);
+        exhaust = true;
+        cardsToPreview = new Normality();
+    }
+
+    @Override
+    public void onUse(AbstractPlayer p, AbstractMonster m) {
+        Boolean[] cons = {false, false, false};
+        for (MapEdge edge : AbstractDungeon.getCurrMapNode().getEdges()) {
+            AbstractRoom room = AbstractDungeon.map.get(edge.dstY).get(edge.dstX).getRoom();
+            if (room instanceof TreasureRoom || room instanceof ShopRoom) {
+                cons[0] = true;
+            } else if (room instanceof MonsterRoom) {
+                cons[1] = true;
+            } else if (room instanceof EventRoom || room instanceof RestRoom) {
+                cons[2] = true;
+            }
+        }
+
+        if (cons[0]) {
+            RelicEventHelper.gainGold(magicNumber * 10);
+            addToBot(new MakeTempCardInDrawPileAction(new Normality(), 1, false, false, true));
+        }
+        if (cons[1]) {
+            addToBot(new DrawCardAction(p, magicNumber));
+        }
+        if (cons[2]) {
+            addToBot(new GainEnergyAction(magicNumber));
+        }
+    }
+    
+    public static void checkUnlockSign(){
+        SignatureHelper.unlock(HSRMod.makePath(ID), true);
+    }
+}
